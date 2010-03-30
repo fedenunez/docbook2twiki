881999b44062f373252d6c69b0abe42a89fa7656
baoqiu <baoqiu@0e703734-2bf1-11de-958b-d3ca58845339>
2010-03-30 02:50:44 +0000

    Added variable org-export-docbook2twiki-xsl-file.

--- trunk/org-twiki.el
+++ trunk/org-twiki.el
@@ -10,6 +10,18 @@
 ;; $Id$
 ;;
 
+(defvar org-export-docbook2twiki-xsl-file
+  "docbook2twiki.xsl"
+  "The full path of file docbook2twiki.xsl.
+The default value is ``docbook2twiki.xsl'', which assumes that
+the file exists in the current working directory, where your Org
+files to be exported exist.")
+
+;; Set org-export-docbook2twiki-xsl-file based on where you saved
+;; docbook2twiki.xsl.
+(setq org-export-docbook2twiki-xsl-file
+      "/Users/bcui/docbook2twiki/docbook2twiki.xsl")
+
 (defun org-export-as-twiki ()
   "Export Org file to TWiki."
   (interactive)
@@ -22,8 +34,10 @@
     (and (file-exists-p twiki-file) (delete-file twiki-file))
     (message "Processing DocBook XML file...")
     (shell-command
-     (format "xsltproc --output %s /Users/bcui/docbook2twiki/docbook2twiki.xsl %s"
-	     twiki-file (shell-quote-argument filename)))
+     (format "xsltproc --output %s %s %s"
+	     twiki-file
+	     org-export-docbook2twiki-xsl-file
+	     (shell-quote-argument filename)))
     (message "Processing DocBook file...done")
     (if (not (file-exists-p twiki-file))
 	(error "TWiki file was not produced")
